ab2f6c91ea8f54d2b1bdbb90e89c630504f52bd3
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-03-11 22:55:54 +0800

    use `pad` instead of zeroPad

--- src/podcast.ts
+++ src/podcast.ts
@@ -1,6 +1,6 @@
 import { el } from '@zero-dependency/dom'
+import { pad } from '@zero-dependency/utils'
 import { storage } from './storage.js'
-import { zeroPad } from './utils.js'
 
 export function patchPodcasts(podcastsElements: HTMLElement[]): void {
   for (const podcast of storage.values) {
@@ -13,9 +13,7 @@ export function patchPodcasts(podcastsElements: HTMLElement[]): void {
     const hours = Math.floor(time / 3600)
     const minutes = Math.floor((time - hours * 3600) / 60)
     const seconds = Math.floor(time - hours * 3600 - minutes * 60)
-    const timeString = `/ ${zeroPad(hours)}:${zeroPad(minutes)}:${zeroPad(
-      seconds
-    )}`
+    const timeString = `/ ${pad(hours)}:${pad(minutes)}:${pad(seconds)}`
 
     const timeElement = el('span', timeString)
     podcastElement.appendChild(timeElement)
--- src/utils.ts
+++
@@ -1,3 +0,0 @@
-export function zeroPad(num: number): string {
-  return num.toString().padStart(2, '0')
-}
