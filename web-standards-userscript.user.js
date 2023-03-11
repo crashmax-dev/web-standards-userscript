33ccd00ec1b9f73e7318d9e815fe116b6fa3a376
crashmax-dev <crashmax.dev@gmail.com>
2023-03-11 14:59:44 +0000

    Deploying to gh-pages from @ crashmax-dev/web-standards-userscript@d1ae83e1e6963a0fe150723eeeee286a3cc39e97 🚀

--- web-standards-userscript.user.js
+++ web-standards-userscript.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        web-standards-userscript
-// @version     0.0.1
+// @version     0.1.0
 // @license     MIT
 // @homepage    https://crashmax-dev.github.io/web-standards-userscript/
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=web-standards.ru
